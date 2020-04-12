52cc08e63a54de3742375b93c50fb8990b9c4431
Alan Rodrigues <alancrato@hotmail.com>
2020-04-12 16:21:24 -0300

    status bar full screen false

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,6 +54,7 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.statusBar.overlaysWebView(false);
       this.splashScreen.hide();
     });
   }
